ccf9def676147bd4ac0cbe99065601f8e996b90c
Md Mahmudul Hasan <mahmudulcsejnu@gmail.com>
2021-10-23 23:13:26 +0600

    Added Necessary models

---
+++ Blog.Models/Account/ApplicationUserIdentity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Models.Account
+{
+    public class ApplicationUserIdentity
+    {
+        public int ApplicationUserId { get; set; }
+        public string Username { get; set; }
+        public string NormalizedUsername { get; set; }
+        public string Email { get; set; }
+        public string NormalizedEmail { get; set; }
+        public string Fullname { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
---
+++ Blog.Models/Blog/PageResults.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Models.Blog
+{
+   public class PageResults<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int TotalCOunt { get; set; }
+    }
+}
