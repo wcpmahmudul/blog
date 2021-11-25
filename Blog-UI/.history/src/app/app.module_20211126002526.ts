893b4ceebdf84927a30505b7b58b5077544841de
Md Mahmudul Hasan <mahmudulcsejnu@gmail.com>
2021-11-26 00:31:22 +0600

    Components added

---
+++ Blog-UI/.history/src/app/app.module_20211126002526.ts
@@ -0,0 +1,22 @@
+Eimport { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SummaryPipe } from './pipes/summary.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SummaryPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
